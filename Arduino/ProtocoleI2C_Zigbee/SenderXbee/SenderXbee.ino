07d9492fa017462a9aa88bc5b7f57ad35fb95110
Wellan72 <77684761+Wellan72@users.noreply.github.com>
2021-02-02 17:37:00 +0100

    Add files via upload

--- Arduino/ProtocoleI2C_Zigbee/SenderXbee/SenderXbee.ino
+++ Arduino/ProtocoleI2C_Zigbee/SenderXbee/SenderXbee.ino
@@ -1,29 +1,25 @@
-/*   ~ Simple Arduino - xBee Transmitter sketch ~
 
-  Read an analog value from potentiometer, then convert it to PWM and finally send it through serial port to xBee.
-  The xBee serial module will send it to another xBee (resiver) and an Arduino will turn on (fade) an LED.
-  The sending message starts with '<' and closes with '>' symbol. 
-  
-  Dev: Michalis Vasilakis // Date:2/3/2016 // Info: www.ardumotive.com // Licence: CC BY-NC-SA                    */
-
-//Constants: 
-const int potPin = A0; //Pot at Arduino A0 pin 
-//Variables:
-int value='a' ; //Value from pot
-
-void setup() {
-  //Start the serial communication
-  Serial.begin(9600); //Baud rate must be the same as is on xBee module
+#include <SoftwareSerial.h>
+SoftwareSerial xbee(2, 3);
+int count;
+ 
+void setup()
+{
+    xbee.begin(9600);                 
+    Serial.begin(9600);                     
+    count = 0;
 }
-
-void loop() {
-  //Read the analog value from pot and store it to "value" variable
+ 
+/***************************************
+*  MAIN LOOP
+***************************************/
+ 
+void loop()
+{
+  String phrase = "Salut mon pote";
   
-  //Map the analog value to pwm value
+  Serial.println(phrase);
+  xbee.println(phrase);
   
-  //Send the message:
-  Serial.print('<');  //Starting symbol
-  Serial.print(value);//Value from 0 to 255
-  Serial.println('>');//Ending symbol
-  value;
+  delay(300);
 }
