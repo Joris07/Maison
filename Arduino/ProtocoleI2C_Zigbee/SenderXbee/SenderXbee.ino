a5bc6c72731b37ef47b09ecf3a4d6e5f687f1ec4
Wellan72 <77684761+Wellan72@users.noreply.github.com>
2021-02-04 17:33:25 +0100

    Update SenderXbee.ino

--- Arduino/ProtocoleI2C_Zigbee/SenderXbee/SenderXbee.ino
+++ Arduino/ProtocoleI2C_Zigbee/SenderXbee/SenderXbee.ino
@@ -1,25 +1,30 @@
 
-#include <SoftwareSerial.h>
-SoftwareSerial xbee(2, 3);
-int count;
- 
-void setup()
-{
-    xbee.begin(9600);                 
-    Serial.begin(9600);                     
-    count = 0;
-}
- 
-/***************************************
-*  MAIN LOOP
-***************************************/
- 
-void loop()
-{
-  String phrase = "Salut mon pote";
-  
-  Serial.println(phrase);
-  xbee.println(phrase);
-  
-  delay(300);
-}
+   #include <Adafruit_AHTX0.h>
+    #include <SoftwareSerial.h>
+    SoftwareSerial xbee(2, 3);
+     
+    Adafruit_AHTX0 aht;
+     
+    void setup() {
+      pinMode(13, OUTPUT);
+      digitalWrite(13, HIGH);
+      xbee.begin(9600); 
+      Serial.begin(9600);
+      Serial.println("Adafruit AHT10/AHT20 demo!");
+     
+      if (! aht.begin()) {
+        Serial.println("Could not find AHT? Check wiring");
+        while (1) delay(10);
+      }
+      Serial.println("AHT10 or AHT20 found");
+    }
+     
+    void loop() {
+      sensors_event_t humidity, temp;
+      aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+      Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+      Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+      xbee.print(temp.temperature);xbee.println(" degrees C");
+      xbee.print(humidity.relative_humidity); xbee.println("% rH");
+      delay(1500);
+    }
