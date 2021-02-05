76d30971a8760ad7a5b38ec6bfabde1585ffa259
Joris07 <77497552+Joris07@users.noreply.github.com>
2021-02-05 17:33:39 +0100

    Update xbee.py

--- Server/xbee.py
+++ Server/xbee.py
@@ -5,7 +5,9 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)
 while 1:
     incoming = ser.readline().strip()
     final= incoming[1:].decode()
-
+    tab=final.split(";")
+    temp=tab[0]
+    humid=tab[1]
     f = open("fichier.txt", "a")
     f.write(final + "\n")
     f.close()
