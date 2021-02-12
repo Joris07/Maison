f10375c4e300874141d9ee35340e59a20f5722bb
Joris07 <77497552+Joris07@users.noreply.github.com>
2021-02-12 14:39:54 +0100

    Update receiver2.py

--- Server/receiver2.py
+++ Server/receiver2.py
@@ -21,26 +21,26 @@ def count_dot(chaine):
     nbr = chaine.count(';')
     return nbr
 
-'''def insert_bdd(nbr, chaine):
+def insert_bdd(nbr, chaine):
     tab=chaine.split(";")
-    col = db.tab[0]
-    for i=1 in range(nbr):'''
-        
-        
+    memo = read_file().split(";")
+    if(tab[0]!=memo[0]):
+        col = db.tab[0][1:]
+        monDico = { }
+        for i in range(1, nbr+1):
+            if(tab[i].find("t")!=-1):
+                monDico["Temperature"] = tab[i][1:]
+            elif(tab[i].find("h")!=-1):
+                monDico["Humidite"] = tab[i][1:]
+        col.insert(monDico)
+        client.close()
+
 def read_file():
     read_temp = open("donnees.txt", "r")
     lines_temp = read_temp.readlines()
     read_temp.close()
     last_line= lines_temp[len(lines_temp)-1].strip()
     return last_line
-    
 
-def insert_aht20():
 
-    doc = { "Temperature":last_line_temperature, "Humidite":last_line_humidity }
-
-    col.insert(doc)
-    client.close()
-
-while 1:    
-    receiver()
+while 1:
