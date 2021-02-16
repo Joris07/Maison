03f670a050407d15078fa84110e67034e44d990f
Wellan72 <77684761+Wellan72@users.noreply.github.com>
2021-02-16 15:09:39 +0100

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,22 @@
 import paho.mqtt.client as mqtt #import the client1
-broker_address="192.168.1.184" 
+import serial
+from pymongo import MongoClient
+broker_address="172.20.78.137"
+x=45 
 #broker_address="iot.eclipse.org" #use external broker
 client = mqtt.Client("P1") #create new instance
 client.connect(broker_address) #connect to broker
-client.publish("house/main-light","OFF")#publish
+client.publish("sensor/temperature",x)#publish
+import serial
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+db = client["db"]
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+    
+while 1:
+    client = mqtt.Client("P1") #create new instance
+    client.connect(broker_address) #connect to broker
+    client.publish("sensor/temperature",x)#publish
