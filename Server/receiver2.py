34d82847d9c215847633f6b9be294e7b5ba5ad6d
Joris07 <77497552+Joris07@users.noreply.github.com>
2021-02-16 16:08:26 +0100

    Update receiver2.py

--- Server/receiver2.py
+++ Server/receiver2.py
@@ -1,4 +1,5 @@
 import serial
+import time
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://127.0.0.1:27017')
@@ -8,6 +9,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 def receiver():
     incoming = ser.readline().strip()
+    time.sleep(2)
     final= incoming[1:].decode()
     return final
 
@@ -20,29 +22,37 @@ def count_dot(chaine):
     nbr = chaine.count(';')
     return nbr
 
-def insert_bdd(nbr, chaine):
+def trier(nbr, chaine):
     tab=chaine.split(";")
     memo = read_file().split(";")
     if(tab[0]!=memo[0]):
-        if(tab[0][1:]=="AHT20"):
-            col = db.AHT20
         monDico = { }
         for i in range(1, nbr+1):
             if(tab[i].find("t")!=-1):
                 monDico["Temperature"] = tab[i][1:]
             elif(tab[i].find("h")!=-1):
                 monDico["Humidite"] = tab[i][1:]
-        col.insert(monDico)
-    client.close()
+        insert_bdd(tab[0][1:], monDico)
+
+def insert_bdd(topic,dico):
+    if(str(topic[1:])=="AHT20"):
+        col = db.AHT20
+        col.insert(dico)
+        client.close()
+    elif(str(topic[1:])=="AHT10"):
+        col = db.AHT10
+        col.insert(dico)
+        client.close()
 
 def read_file():
-    read_temp = open("donnees.txt", "r")
-    lines_temp = read_temp.readlines()
-    read_temp.close()
-    last_line= lines_temp[len(lines_temp)-1].strip()
+    read = open("donnees.txt", "r")
+    lines = read.readlines()
+    read.close()
+    last_line= lines[len(lines)-1].strip()
     return last_line
 
-recu = receiver()
-write_file(recu)
-insert_bdd(count_dot(recu), recu)
+while 1:
+    recu = receiver()
+    trier(count_dot(recu), recu)
+    write_file(recu)
 
