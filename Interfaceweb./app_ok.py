99ab4eab67d52a399d4a642b7cdd17414e0005df
Joris07 <77497552+Joris07@users.noreply.github.com>
2021-03-16 17:34:06 +0100

    Update app_ok.py

--- Interfaceweb./app_ok.py
+++ Interfaceweb./app_ok.py
@@ -17,8 +17,8 @@ def lastValuehumidity():
         return x
 
 def lastValuelm335 ():
-    lmt335_1= db.LMT335.find().sort("temperature", -1).limit(1)
-    for x in lmt335_1:
+    lm335_1= db.LM335.find().sort("temperature", -1).limit(1)
+    for x in lm335_1:
         return x
 
 def collection():
@@ -44,37 +44,46 @@ def stock_temp_AHT20():
     temp=[]
     for i in data:
         date.append(i.get("date"))
-        temp.append(i.get("temperature"))
-    date.remove(None)    
+        temp.append(i.get("temperature"))    
     return (temp,date)
 
-def stocklmt335():
-    data=db.LMT335.find({ "temperature": {"$gt":"#"}})
-    lmt335=[]
-    datelmt335=[]
+def stocklm335():
+    data=db.LM335.find({ "temperature": {"$gt":"#"}})
+    lm335=[]
+    datelm335=[]
     for i in data:
-        datelmt335.append(i.get("date"))
-        lmt335.append(i.get("temperature"))
-    return (lmt335,datelmt335)
+        datelm335.append(i.get("date"))
+        lm335.append(i.get("temperature"))
+    return (lm335,datelm335)
 
 
 @app.route("/")
 def index():
-    temperature=lastValueTemperatrue().get("temperature")
     topic=request.values.get("topic")
     capteur=request.values.get("capteur")
-    humidite=lastValuehumidity().get('humidite')
-    lmt335=lastValuelm335().get('temperature')
     coll = collection()
     if(topic=="AHT20" and capteur=="Temperature"):
         temp,date=stock_temp_AHT20()
+        temperature=lastValueTemperatrue().get("temperature")
+        humidite=0
     elif(topic=="AHT20" and capteur=="Humidite"):
         temp,date=stock_humidity_AHT20()
-    elif(topic=="LMT335" and capteur=="Temperature"):
-        temp,date=stocklmt335()
+        humidite=lastValuehumidity().get('humidite')
+        temperature = 0
+    elif(topic=="LM335" and capteur=="Temperature"):
+        temp,date=stocklm335()
+        temperature=lastValuelm335().get('temperature')
+        humidite=0
+    elif(topic=="AHT20" and capteur=="#"):
+        temp,date=stocklm335()
+        temperature=lastValuelm335().get('temperature')
+        humidite= lastValuehumidity().get('humidite')
     else: 
-        temp,date=stock_temp_AHT20()
-    return render_template('index.html', temperature = temperature, humidite = humidite, lmt335=lmt335, coll=coll, temp=temp,date=date)
+        temp,date=0,0
+        temperature = 0
+        humidite = 0
+    return render_template('index.html', temperature=temperature, humidite=humidite, coll=coll, temp=temp, date=date)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
