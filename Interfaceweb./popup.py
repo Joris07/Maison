57600d5bb0a769016baad402fa78ab3864b3221d
Joris07 <77497552+Joris07@users.noreply.github.com>
2021-03-23 13:46:56 +0100

    Update popup.py

--- Interfaceweb./popup.py
+++ Interfaceweb./popup.py
@@ -7,30 +7,29 @@ window.geometry('150x50')
 
 #Load both the images
 
-label = Label(text="0", font="Arial 30", width=10)
+label = Label(text="0", font="Arial 30", width=10)#Initialise le label 
 label.pack()
 
-# This is the event handler method that receives the Mosquito messages
+#Reçoit les données 
 def on_message(client, userdata, message):
-    msg = str(message.payload.decode("utf-8"))
+    msg = str(message.payload.decode("utf-8"))#Interprete le message recu du topic
     print("message received " , msg)
-    label.configure(text="%s" % msg)
+    label.configure(text="%s" % msg)#Remplace le message par le nouveau dans le label
     print("OK")
-    label.update()
+    label.update()#Met à jour le label 
 
 broker_address="172.20.78.137"
 
 print("creating new instance")
-client = mqtt.Client() #create new instance
-client.on_message=on_message #attach function to callback
+client = mqtt.Client() #Créer une nouvelle instance
+client.on_message=on_message #
 
 print("connecting to broker")
-client.connect(broker_address) #connect to broker
+client.connect(broker_address) #Connexion au broker
 
 print("Subscribing to topic","AHT20/temperature")
-client.subscribe("1AHT20/temperature")
+client.subscribe("1AHT20/temperature")#Souscrit au topic 1AHT20/temperature
 
- #Start the MQTT Mosquito process loop
-client.loop_start() 
+client.loop_start()#Démarre la boucle MQTT
 
 window.mainloop()
